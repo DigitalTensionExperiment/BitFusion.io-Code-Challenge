beb3f5866e6465584725dd5ef6c4dfeeb8b12764
DigitalTensionExperiment <zareenmwilhelm@gmail.com>
2017-02-17 13:36:13 -0600

    Creating restfulFlaskAPI mod;

---
+++ CLI/restfulFlaskAPI.py
@@ -0,0 +1,2 @@
+def cli():
+    print "This is restfulFlaskAPI ;"
--- CLI/setup.py
+++ CLI/setup.py
@@ -3,11 +3,13 @@ from setuptools import setup
 setup(
     name='',
     version='1.0',
-    py_modules=[],
+    py_modules=['restfulFlaskAPI'],
     install_requires=[
         'Click',
+        'Docker',
     ],
     entry_points='''
         [console_scripts]
-    '''
+        restfulFlaskAPI=restfulFlaskAPI:cli
+    ''', 
 )
